2ba26d3289492668d86ca07c916dbbf5d82175c0
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-18 04:12:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// Add your code here
-function submitData(name , email) {
-  let configurationObject = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify({
-    name ,
-    email,
-  })
-}
-    fetch('http://localhost:3000/user' ,configurationObject )
-    .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Bad things! Ragnarők!");
-    console.log(error.message);
-  });
-}
+// // Add your code here
 
+function submitData( name, email ) {
+  let configration = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    };
+    
+  return fetch( 'http://localhost:3000/users', configration )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
 let name = "Steve"
 let email = "steve@steve.com"
 submitData(name , email)
