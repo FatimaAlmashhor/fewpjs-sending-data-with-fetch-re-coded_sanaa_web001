f609dc50bf4c932d0bb8f327e6fe37cd4d9af55b
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-18 04:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// Add your code here
-function submitData(name , email) {
-  let configurationObject = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify({
-    name ,
-    email,
-  })
-}
-    fetch('http://localhost:3000/user' ,configurationObject )
-    then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Bad things! Ragnarők!");
-    console.log(error.message);
-  });
-}
+// // Add your code here
+// function submitData(name , email) {
+//   let configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     name ,
+//     email,
+//   })
+// }
+//     fetch('http://localhost:3000/user' ,configurationObject )
+//     then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object);
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
+// }
 
-let name = "Steve"
-let email = "steve@steve.com"
-submitData(name , email)
+// let name = "Steve"
+// let email = "steve@steve.com"
+// submitData(name , email)
