488a0e6817821aaeddcd19c209ac3a8d4ad4b8ad
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-18 04:12:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+// // Add your code here
+
+function submitData( name, email ) {
+  let configration = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    };
+    
+  return fetch( 'http://localhost:3000/users', configration )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
+let name = "Steve"
+let email = "steve@steve.com"
+submitData(name , email)
